1cc6055ee61caed4d3ffb34c141758e54b6a0599
Manuel Gopar <manuelgop@gmail.com>
2018-03-17 18:37:48 -0600

    Error solucionado

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ var key = 'AIzaSyB97v_oTQxHYyQbXxS8wxiUHOm5DUVlpdY';
 var playlistID = 'PL2fnLUTsNyq7A335zB_RpOzu7hEUcSJbB';
 var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
-var options ={
-    playlistID: playlistID,
+var options = {
     part: 'snippet',
     key: key,
     maxResults: 20,
-    
+    playlistId: playlistID
 }
+
 loadVids();
 
 function loadVids() {
-    console.log('TEEEEESTEO');
-    
-    $.getJSON(URL,options, function (data) {
-        console.log(data)  
-       })
-    
+    $.getJSON(URL, options, function (data) {
+        //var id = data.items[0].snippet.resourceId.videoId;
+       // mainVid(id);
+       // resultsLoop(data);
+       console.log(data);
+    });
 }
 
 function mainVid(id) {
